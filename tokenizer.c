5c98e5908827366745382fb3a938b5a7888c1713
Josef Edwards <joed6834@colorado.edu>
2024-11-11 20:58:00 -0500

    Update tokenizer.c
    Signed-off-by: Josef Edwards <joed6834@colorado.edu>

--- tokenizer.c
+++ tokenizer.c
@@ -1,28 +1,38 @@
+#ifndef TOKENIZER_H
+#define TOKENIZER_H
+
+void tokenize(char *input);
+
+#endif // TOKENIZER_H
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "tokenizer.h"
 
 #define MAX_TOKEN_SIZE 100
 
 void tokenize(char *input) {
-    char *token = strtok(input, " \t\n");  // Delimiters: space, tab, newline
+    char *token = strtok(input, " \t\n");
     while (token != NULL) {
         printf("Token: %s\n", token);
         token = strtok(NULL, " \t\n");
     }
 }
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "tokenizer.h"
+
+#define MAX_TOKEN_SIZE 100
 
 int main() {
     char input[MAX_TOKEN_SIZE];
 
-    // Get input string
     printf("Enter a string to tokenize: ");
     fgets(input, MAX_TOKEN_SIZE, stdin);
-
-    // Remove newline character
     input[strcspn(input, "\n")] = '\0';
 
-    // Tokenize the input string
     tokenize(input);
 
     return 0;
