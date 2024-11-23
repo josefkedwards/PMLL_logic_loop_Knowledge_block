73c849be9e504963d36f00c8d136d60ebd16e43f
Josef Edwards <joed6834@colorado.edu>
2024-11-23 09:29:55 -0500

    Update pml_logic_loop.h
    updated definitions... lol
    
    Signed-off-by: Josef Edwards <joed6834@colorado.edu>

--- pml_logic_loop.h
+++ pml_logic_loop.h
@@ -1,9 +1,9 @@
-#ifndef PML_H
-#define PML_H
+#ifndef PML_LOGIC_LOOP_H
+#define PML_LOGIC_LOOP_H
 
 #include <stddef.h>
 
-// Define the PMLL structure
+// Define the Persistent Memory Logic Loop (PMLL) structure
 typedef struct PMLL {
     int user_adoption_rate;
     int security_incident_rate;
@@ -18,8 +18,9 @@ typedef struct PMLL {
     int behavioral_analytics;
     int security_compliance;
 
-    void* model;
+    void* model; // Placeholder for the underlying machine learning model
 
+    // Function pointers for PMLL operations
     void (*predict)(struct PMLL*);
     void (*train)(struct PMLL*);
     void (*deploy)(struct PMLL*);
@@ -30,16 +31,60 @@ typedef struct PMLL {
 } PMLL;
 
 // Tokenizer Functions
+/**
+ * Tokenizes UTF-8 input and applies a callback function to each token.
+ * @param input The UTF-8 encoded input string.
+ * @param callback A function to handle each token.
+ */
 void utf8_tokenize(const char* input, void (*callback)(const char* token));
 
 // PMLL Logic Functions
-PMLL* pml_init();
-void predict(PMLL* pml);
-void train(PMLL* pml);
-void deploy(PMLL* pml);
-void monitor(PMLL* pml);
-void maintain(PMLL* pml);
-void automate(PMLL* pml);
-void evaluate(PMLL* pml);
+/**
+ * Initializes a new Persistent Memory Logic Loop (PMLL) instance.
+ * @return A pointer to the initialized PMLL instance.
+ */
+PMLL* pml_logic_loop_init();
 
-#endif // PML_H
+/**
+ * Predict function for the PMLL instance.
+ * @param pml A pointer to the PMLL instance.
+ */
+void pml_logic_loop_predict(PMLL* pml);
+
+/**
+ * Train function for the PMLL instance.
+ * @param pml A pointer to the PMLL instance.
+ */
+void pml_logic_loop_train(PMLL* pml);
+
+/**
+ * Deploy function for the PMLL instance.
+ * @param pml A pointer to the PMLL instance.
+ */
+void pml_logic_loop_deploy(PMLL* pml);
+
+/**
+ * Monitor function for the PMLL instance.
+ * @param pml A pointer to the PMLL instance.
+ */
+void pml_logic_loop_monitor(PMLL* pml);
+
+/**
+ * Maintain function for the PMLL instance.
+ * @param pml A pointer to the PMLL instance.
+ */
+void pml_logic_loop_maintain(PMLL* pml);
+
+/**
+ * Automate function for the PMLL instance.
+ * @param pml A pointer to the PMLL instance.
+ */
+void pml_logic_loop_automate(PMLL* pml);
+
+/**
+ * Evaluate function for the PMLL instance.
+ * @param pml A pointer to the PMLL instance.
+ */
+void pml_logic_loop_evaluate(PMLL* pml);
+
+#endif // PML_LOGIC_LOOP_H
