53e5ae42f9a853383eb52d5148ee4d4e7cbbc542
Josef Edwards <joed6834@colorado.edu>
2024-12-24 05:28:38 -0500

    Create GPU.h
    Tiny header 
    
    Signed-off-by: Josef Edwards <joed6834@colorado.edu>

---
+++ GPU.h
@@ -0,0 +1,13 @@
+#ifndef GPU_H
+#define GPU_H
+
+#define BLOCK_SIZE 256
+#define MAX_CLUSTERS 9
+#define MAX_ITERATIONS 1024
+#define MAX_NODES 1024
+#define INF 1e9
+
+// Function prototypes
+void RunLogicLoop();
+
+#endif // GPU_H
