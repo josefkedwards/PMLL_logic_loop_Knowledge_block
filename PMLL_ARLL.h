2e39682ee662a156651877417ec1f0a0fcc7f3ca
Josef Edwards <joed6834@colorado.edu>
2024-12-24 06:53:41 -0500

    Update PMLL_ARLL.h
    header
    
    Signed-off-by: Josef Edwards <joed6834@colorado.edu>

--- PMLL_ARLL.h
+++ PMLL_ARLL.h
@@ -2,6 +2,7 @@
 #define PMLL_ARLL_H
 
 #include <jansson.h> // JSON processing library
+#include <time.h>    // For timestamp functionality
 
 // Constants
 #define RETRY_LIMIT 5
@@ -16,19 +17,11 @@ typedef struct {
     json_t *json;           // Parsed JSON object
 } PMLL_ARLL_State;
 
-// Initialize the PMLL_ARLL state
+// Function prototypes
 void pmll_arll_init(PMLL_ARLL_State *state, int max_retries);
-
-// Process a chunk of data
 int pmll_arll_process_chunk(PMLL_ARLL_State *state, const char *chunk, size_t chunk_size);
-
-// Write valid data to the knowledge graph
 void pmll_arll_write_to_knowledge_graph(PMLL_ARLL_State *state);
-
-// Perform cleanup of the PMLL_ARLL state
 void pmll_arll_cleanup(PMLL_ARLL_State *state);
-
-// Log messages with a timestamp
 void log_message(const char *level, const char *message);
 
 #endif // PMLL_ARLL_H
