46fdfb99f28023006730caaa8ca95b7ce90442f1
Josef Edwards <joed6834@colorado.edu>
2024-12-24 05:39:20 -0500

    Update API_Llama.c
    Signed-off-by: Josef Edwards <joed6834@colorado.edu>

--- API_Llama.c
+++ API_Llama.c
@@ -1,187 +1,95 @@
+#include "API.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+#include <unistd.h> // For close()
 
-#define API_URL "localhost"
-#define API_PORT 8080
-#define CROSS_TALK_URL "localhost"
-#define CROSS_TALK_PORT 8081
-#define PMLL_URL "localhost"
-#define PMLL_PORT 8082
-#define GITHUB_API_URL "api.github.com"
-#define GITHUB_API_PORT 443
+// Function to handle PMLL logic
+void handle_pmll_logic(int sockfd) {
+    const char* request = "GET /pmll/logic HTTP/1.1\r\nHost: localhost:8082\r\n\r\n";
+    char response[BUFFER_SIZE];
 
-// Function to create a new socket
-int create_socket() {
-    int sockfd;
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) {
-        perror("socket");
-        exit(1);
-    }
-    return sockfd;
-}
+    printf("Sending PMLL Logic Request...\n");
+    send_request(sockfd, request);
+    receive_response(sockfd, response);
+    printf("PMLL Response: %s\n", response);
 
-// Function to set up the server address
-void set_up_server_address(struct sockaddr_in* server_addr, char* url, int port) {
-    server_addr->sin_family = AF_INET;
-    server_addr->sin_port = htons(port);
-    inet_pton(AF_INET, url, &server_addr->sin_addr);
-}
-
-// Function to connect to the API server
-void connect_to_api_server(int sockfd, struct sockaddr_in* server_addr) {
-    if (connect(sockfd, (struct sockaddr*)server_addr, sizeof(*server_addr)) < 0) {
-        perror("connect");
-        exit(1);
+    // Add PMLL-specific processing here
+    if (strstr(response, "SUCCESS")) {
+        printf("PMLL Logic executed successfully.\n");
+    } else {
+        printf("PMLL Logic failed or returned an unknown state.\n");
     }
 }
 
-// Function to connect to the cross-talk server
-void connect_to_cross_talk_server(int sockfd, struct sockaddr_in* server_addr) {
-    if (connect(sockfd, (struct sockaddr*)server_addr, sizeof(*server_addr)) < 0) {
-        perror("connect");
-        exit(1);
+// Function to handle ARLL logic
+void handle_arll_logic(int sockfd) {
+    const char* request = "GET /arll/logic HTTP/1.1\r\nHost: localhost:8083\r\n\r\n";
+    char response[BUFFER_SIZE];
+
+    printf("Sending ARLL Logic Request...\n");
+    send_request(sockfd, request);
+    receive_response(sockfd, response);
+    printf("ARLL Response: %s\n", response);
+
+    // Add ARLL-specific processing here
+    if (strstr(response, "REWARD")) {
+        printf("ARLL rewarded successfully.\n");
+    } else {
+        printf("ARLL Logic failed or returned an unknown state.\n");
     }
 }
 
-// Function to connect to the PMLL server
-void connect_to_pmll_server(int sockfd, struct sockaddr_in* server_addr) {
-    if (connect(sockfd, (struct sockaddr*)server_addr, sizeof(*server_addr)) < 0) {
-        perror("connect");
-        exit(1);
+// Function to handle EFLL logic
+void handle_efll_logic(int sockfd) {
+    const char* request = "GET /efll/logic HTTP/1.1\r\nHost: localhost:8084\r\n\r\n";
+    char response[BUFFER_SIZE];
+
+    printf("Sending EFLL Logic Request...\n");
+    send_request(sockfd, request);
+    receive_response(sockfd, response);
+    printf("EFLL Response: %s\n", response);
+
+    // Add EFLL-specific processing here
+    if (strstr(response, "CLEAN")) {
+        printf("EFLL verified memory is clean.\n");
+    } else if (strstr(response, "CORRUPT")) {
+        printf("EFLL detected memory corruption!\n");
+    } else {
+        printf("EFLL Logic returned an unknown state.\n");
     }
 }
 
-// Function to connect to the GitHub API server
-void connect_to_github_api_server(int sockfd, struct sockaddr_in* server_addr) {
-    if (connect(sockfd, (struct sockaddr*)server_addr, sizeof(*server_addr)) < 0) {
-        perror("connect");
-        exit(1);
-    }
-}
+// Main function for cascading Llama logic
+void run_llama_cascade() {
+    int pmll_sockfd, arll_sockfd, efll_sockfd;
+    struct sockaddr_in pmll_server_addr, arll_server_addr, efll_server_addr;
 
-// Function to send a request to the API server
-void send_request_to_api_server(int sockfd, char* request) {
-    send(sockfd, request, strlen(request), 0);
-}
-
-// Function to receive a response from the API server
-void receive_response_from_api_server(int sockfd, char* response) {
-    recv(sockfd, response, 1024, 0);
-}
-
-// Function to send a request to the cross-talk server
-void send_request_to_cross_talk_server(int sockfd, char* request) {
-    send(sockfd, request, strlen(request), 0);
-}
-
-// Function to receive a response from the cross-talk server
-void receive_response_from_cross_talk_server(int sockfd, char* response) {
-    recv(sockfd, response, 1024, 0);
-}
-
-// Function to send a request to the PMLL server
-void send_request_to_pmll_server(int sockfd, char* request) {
-    send(sockfd, request, strlen(request), 0);
-}
-
-// Function to receive a response from the PMLL server
-void receive_response_from_pmll_server(int sockfd, char* response) {
-    recv(sockfd, response, 1024, 0);
-}
-
-// Function to send a request to the GitHub API server
-void send_request_to_github_api_server(int sockfd, char* request) {
-    send(sockfd, request, strlen(request), 0);
-}
-
-// Function to receive a response from the GitHub API server
-void receive_response_from_github_api_server(int sockfd, char* response) {
-    recv(sockfd, response, 1024, 0);
-}
-
-int main() {
-    int api_sockfd, cross_talk_sockfd, pmll_sockfd, github_api_sockfd;
-    struct sockaddr_in api_server_addr, cross_talk_server_addr, pmll_server_addr, github_api_server_addr;
-
-    // Create a new socket for the API server
-    api_sockfd = create_socket();
-
-    // Set up the server address for the API server
-    set_up_server_address(&api_server_addr, API_URL, API_PORT);
-
-    // Connect to the API server
-    connect_to_api_server(api_sockfd, &api_server_addr);
-
-    // Create a new socket for the cross-talk server
-    cross_talk_sockfd = create_socket();
-
-    // Set up the server address for the cross-talk server
-    set_up_server_address(&cross_talk_server_addr, CROSS_TALK_URL, CROSS_TALK_PORT);
-
-    // Connect to the cross-talk server
-    connect_to_cross_talk_server(cross_talk_sockfd, &cross_talk_server_addr);
-
-    // Create a new socket for the PMLL server
+    // Create and connect to PMLL server
     pmll_sockfd = create_socket();
-
-    // Set up the server address for the PMLL server
     set_up_server_address(&pmll_server_addr, PMLL_URL, PMLL_PORT);
+    connect_to_server(pmll_sockfd, &pmll_server_addr);
 
-    // Connect to the PMLL server
-    connect_to_pmll_server(pmll_sockfd, &pmll_server_addr);
+    // Create and connect to ARLL server
+    arll_sockfd = create_socket();
+    set_up_server_address(&arll_server_addr, ARLL_URL, ARLL_PORT);
+    connect_to_server(arll_sockfd, &arll_server_addr);
 
-    // Create a new socket for the GitHub API server
-    github_api_sockfd = create_socket();
+    // Create and connect to EFLL server
+    efll_sockfd = create_socket();
+    set_up_server_address(&efll_server_addr, EFLL_URL, EFLL_PORT);
+    connect_to_server(efll_sockfd, &efll_server_addr);
 
-    // Set up the server address for the GitHub API server
-    set_up_server_address(&github_api_server_addr, GITHUB_API_URL, GITHUB_API_PORT);
+    // Execute logic loops
+    printf("\nStarting Llama Logic Cascade...\n");
+    handle_pmll_logic(pmll_sockfd);
+    handle_arll_logic(arll_sockfd);
+    handle_efll_logic(efll_sockfd);
 
-    // Connect to the GitHub API server
-    connect_to_github_api_server(github_api_sockfd, &github_api_server_addr);
-
-    // Send a request to the API server
-    char* request = "GET /api/endpoint HTTP/1.1\r\nHost: localhost:8080\r\n\r\n";
-    send_request_to_api_server(api_sockfd, request);
-
-    // Receive a response from the API server
-    char response[1024];
-    receive_response_from_api_server(api_sockfd, response);
-    printf("%s", response);
-
-    // Send a request to the cross-talk server
-    request = "GET /cross-talk/endpoint HTTP/1.1\r\nHost: localhost:8081\r\n\r\n";
-    send_request_to_cross_talk_server(cross_talk_sockfd, request);
-
-    // Receive a response from the cross-talk server
-    receive_response_from_cross_talk_server(cross_talk_sockfd, response);
-    printf("%s", response);
-
-    // Send a request to the PMLL server
-    request = "GET /pmll/endpoint HTTP/1.1\r\nHost: localhost:8082\r\n\r\n";
-    send_request_to_pmll_server(pmll_sockfd, request);
-
-    // Receive a response from the PMLL server
-    receive_response_from_pmll_server(pmll_sockfd, response);
-    printf("%s", response);
-
-    // Send a request to the GitHub API server
-    request = "GET /repos/bearycool11/pmll HTTP/1.1\r\nHost: api.github.com\r\n\r\n";
-    send_request_to_github_api_server(github_api_sockfd, request);
-
-    // Receive a response from the GitHub API server
-    receive_response_from_github_api_server(github_api_sockfd, response);
-    printf("%s", response);
-
-    // Close the sockets
-    close(api_sockfd);
-    close(cross_talk_sockfd);
+    // Close sockets
     close(pmll_sockfd);
-    close(github_api_sockfd);
+    close(arll_sockfd);
+    close(efll_sockfd);
 
-    return 0;
+    printf("\nLlama Logic Cascade completed.\n");
 }
