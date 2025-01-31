21fdf4447dfa098d44f15c02bdcd9d957bf27a64
josefkedwards <cedwards19606389@yahoo.com>
2025-01-31 17:01:29 -0500

    Update Persistence.h
    Signed-off-by: josefkedwards <cedwards19606389@yahoo.com>

--- Persistence.h
+++ Persistence.h
@@ -3,19 +3,25 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "memory_silo.h"
 #include "PMLL_ARLL_EFLL.h"
+#include "knowledge.h"
 
-// Assuming these types are defined in other headers:
-typedef struct io_socket_t io_socket_t;
-typedef struct PMLL_ARLL_EFLL_State PMLL_ARLL_EFLL_State;
-typedef struct Graph Graph;
+#define MAX_KNOWLEDGE_NODES 4096
+#define SESSION_TIMEOUT_MS 5000
+#define ENCRYPTION_KEY_SIZE 2048
 
-// Structure to encapsulate session context for persistence
+typedef struct io_socket_t io_socket_t; // Forward declaration
+typedef struct Graph Graph; // Forward declaration
+struct RSA; // Forward declaration, assuming RSA is defined elsewhere
+
 typedef struct {
     io_socket_t io_socket;
     PMLL_ARLL_EFLL_State state;
     Graph *knowledge_graph;
+    struct RSA *encryption_key;
+    memory_silo_t *persistent_silo;
 } SessionContext;
 
 // Function to save the state of the memory silo
@@ -24,10 +30,16 @@ void save_memory_silo(const char *filename, memory_silo_t *memory_silo);
 // Function to load the state of the memory silo
 memory_silo_t *load_memory_silo(const char *filename);
 
-// Function to save session state
-void save_session_state(const char *filename, SessionContext *ctx);
+// Function to save session state with encryption
+void save_session_state(SessionContext *ctx, const char *filename);
 
-// Function to load session state
-void load_session_state(const char *filename, SessionContext *ctx);
+// Function to load session state with decryption
+void load_session_state(SessionContext *ctx, const char *filename);
+
+// Helper function to calculate checksum
+unsigned long calculate_checksum(const unsigned char *data, size_t size);
+
+// Helper function to write file atomically
+int write_file_atomically(const char *filename, const unsigned char *data, size_t size);
 
 #endif // PERSISTENCE_H
