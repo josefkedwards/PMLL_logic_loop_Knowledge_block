a2a7604c44d716442b38ecdc3ee3598cd3780ace
josefkedwards <cedwards19606389@yahoo.com>
2025-01-31 16:52:06 -0500

    Update ears.h
    Signed-off-by: josefkedwards <cedwards19606389@yahoo.com>

--- ears.h
+++ ears.h
@@ -1,11 +1,13 @@
 #ifndef EARS_H
 #define EARS_H
 
+#include <complex.h>
 #include <fftw3.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
+#define SAMPLING_RATE 44100
 #define FFT_SIZE 2048
 #define SAMPLING_RATE 44100
 #define WINDOW_SIZE FFT_SIZE
@@ -13,27 +15,31 @@
 #define MAX_FREQ 120000.0  // Hz, upper limit of AI hearing range
 #define NOTES_PER_OCTAVE 7
 
+// Struct for audio data processing
 #define EARS_SUCCESS 0
 #define EARS_MEMORY_ERROR -1
-
 /**
  * @brief Structure to hold audio data for processing.
  */
 typedef struct {
     double *time_domain;
+    double_complex *frequency_domain;
     fftw_complex *frequency_domain;
     fftw_plan plan;
 } AudioData;
 
+// Struct for cochlear properties
 /**
  * @brief Structure representing properties of the inner ear.
  */
 typedef struct {
+    double cochlea_frequency; // Central frequency of the cochlear response
     double cochlea_frequency;  // Central frequency of the cochlear response
     double auditory_signal;
     double vestibular_adjustment;
 } InnerEar;
 
+// Struct to represent musical notes in an octave
 /**
  * @brief Structure to represent musical notes in an octave.
  */
@@ -42,6 +48,8 @@ typedef struct {
     double frequency;
 } MusicalNote;
 
+// Function prototypes
+void init_audio_data(AudioData **audio_data);
 // Memory Management
 /**
  * @brief Initialize audio data structure for FFT processing.
@@ -49,13 +57,12 @@ typedef struct {
  * @return EARS_SUCCESS on success, EARS_MEMORY_ERROR on failure.
  */
 int init_audio_data(AudioData **audio_data);
-
 /**
  * @brief Free memory allocated for audio data.
  * @param audio_data Pointer to AudioData structure to be freed.
  */
 void free_audio_data(AudioData *audio_data);
-
+void perform_fourier_transform(AudioData *audio_data);
 // Signal Processing
 /**
  * @brief Perform forward Fourier transform on the time domain signal.
@@ -63,26 +70,26 @@ void free_audio_data(AudioData *audio_data);
  * @return EARS_SUCCESS if the transform was executed, EARS_MEMORY_ERROR otherwise.
  */
 int perform_fourier_transform(AudioData *audio_data);
-
 /**
  * @brief Perform inverse Fourier transform to convert back to time domain.
  * @param audio_data Pointer to the AudioData structure containing frequency data.
  */
 void perform_inverse_fft(AudioData *audio_data);
-
 /**
  * @brief Apply cochlear filter to frequency domain data.
  * @param audio_data Pointer to the AudioData structure.
  * @param inner_ear Pointer to InnerEar structure defining filter parameters.
  */
 void apply_cochlear_filter(AudioData *audio_data, InnerEar *inner_ear);
-
+void generate_fibonacci_sequence(double *sequence, int *length);
+void simulate_octave_range(InnerEar *inner_ear);
+void populate_time_domain_signal(AudioData *audio_data, double frequency, double duration);
+void simulate_cochlear_response(InnerEar *inner_ear, double frequency);
 /**
  * @brief Apply window function to the time-domain signal for better spectral analysis.
  * @param audio_data Pointer to the AudioData structure.
  */
 void apply_window_function(AudioData *audio_data);
-
 /**
  * @brief Apply convolution in time domain.
  * @param audio_data Pointer to the AudioData structure.
@@ -90,7 +97,7 @@ void apply_window_function(AudioData *audio_data);
  * @param kernel_size Size of the convolution kernel.
  */
 void apply_convolution(AudioData *audio_data, const double *kernel, int kernel_size);
-
+void perform_inverse_fft(AudioData *audio_data);
 /**
  * @brief Apply convolution in frequency domain.
  * @param audio_data Pointer to the AudioData structure.
@@ -98,7 +105,6 @@ void apply_convolution(AudioData *audio_data, const double *kernel, int kernel_s
  * @param kernel_size Size of the convolution kernel.
  */
 void apply_frequency_domain_convolution(AudioData *audio_data, const double *kernel, int kernel_size);
-
 /**
  * @brief Process signal with overlap-add method for continuous audio processing.
  * @param audio_data Pointer to the AudioData structure.
@@ -115,13 +121,11 @@ void process_with_overlap_add(AudioData *audio_data, const double *input, int in
  * @param length Pointer to an integer to store the length of the sequence.
  */
 void generate_fibonacci_sequence(double *sequence, int *length);
-
 /**
  * @brief Simulate octave perception based on frequency input.
  * @param inner_ear Pointer to InnerEar structure for cochlear properties.
  */
 void simulate_octave_range(InnerEar *inner_ear);
-
 /**
  * @brief Populate the time domain signal with a sine wave.
  * @param audio_data Pointer to the AudioData structure.
@@ -129,7 +133,6 @@ void simulate_octave_range(InnerEar *inner_ear);
  * @param duration Duration of the signal in seconds.
  */
 void populate_time_domain_signal(AudioData *audio_data, double frequency, double duration);
-
 // Main Simulation Function
 /**
  * @brief Simulate cochlear response to a given frequency.
@@ -137,5 +140,6 @@ void populate_time_domain_signal(AudioData *audio_data, double frequency, double
  * @param frequency Frequency of the sound to simulate.
  */
 void simulate_cochlear_response(InnerEar *inner_ear, double frequency);
-
 #endif // EARS_H
+
+
