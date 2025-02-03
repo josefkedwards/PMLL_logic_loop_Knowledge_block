fd4b079d76885589e023a524689cef47b23c4180
josefkedwards <cedwards19606389@yahoo.com>
2025-02-03 14:52:00 -0500

    Update pml_logic_loop.h
    There are going to be duplicates. Give me a second to refactor and combine 
    
    Signed-off-by: josefkedwards <cedwards19606389@yahoo.com>

--- pml_logic_loop.h
+++ pml_logic_loop.h
@@ -146,3 +146,64 @@ void pml_logic_loop_automate(PMLL* pml);
 void pml_logic_loop_evaluate(PMLL* pml);
 
 #endif // PML_LOGIC_LOOP_H
+#ifndef PML_LOGIC_LOOP_H
+#define PML_LOGIC_LOOP_H
+
+#include <stddef.h>
+
+// 📡 Supported AI Models
+typedef enum {
+    AI_NONE,
+    AI_COPILOT,
+    AI_LLAMA,
+    AI_GPT_4o,
+    AI_GPT_o1,
+    AI_GPT_o3,
+    AI_GPT_o3_MINI_HIGH,
+    AI_GPT_o4, // Future GPT-5o
+    AI_CUSTOM_MODEL
+} AI_Model_Type;
+
+// 🧠 Persistent Memory Logic Loop (PMLL) Structure
+typedef struct PMLL {
+    // 📊 AI System Performance Metrics
+    int user_adoption_rate;
+    int security_incident_rate;
+    int user_satisfaction_rate;
+    int authentication_success_rate;
+    int authorization_success_rate;
+    int data_protection_effectiveness;
+    int response_time;
+    int system_performance;
+    int user_engagement;
+    int feature_utilization;
+    int behavioral_analytics;
+    int security_compliance;
+
+    // 📖 AI Knowledge Graph (Persistent Memory)
+    char knowledge_graph[8192];
+
+    // 🤖 AI Model Pointer (Can be dynamically set)
+    AI_Model_Type selected_ai_model;
+
+    // 🔄 Function Pointers for PMLL Operations
+    void (*predict)(struct PMLL*);
+    void (*train)(struct PMLL*);
+    void (*deploy)(struct PMLL*);
+    void (*monitor)(struct PMLL*);
+    void (*maintain)(struct PMLL*);
+    void (*automate)(struct PMLL*);
+    void (*evaluate)(struct PMLL*);
+} PMLL;
+
+// 📌 PMLL Core Functions
+PMLL* pml_logic_loop_init(AI_Model_Type model_type);
+void pml_logic_loop_predict(PMLL* pml);
+void pml_logic_loop_train(PMLL* pml);
+void pml_logic_loop_deploy(PMLL* pml);
+void pml_logic_loop_monitor(PMLL* pml);
+void pml_logic_loop_maintain(PMLL* pml);
+void pml_logic_loop_automate(PMLL* pml);
+void pml_logic_loop_evaluate(PMLL* pml);
+
+#endif // PML_LOGIC_LOOP_H
